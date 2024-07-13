1a56d6821293664bf03337611e6d05ebcbba5c6f
yahya-mohamoud <yahyamohamoud625@gmail.com.com>
2024-07-13 21:01:18 +0300

    edited all the files fisnished the project

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # etch-A-sketch
+A simple etch-a-sketch project using HTML, CSS, and JavaScript.
+
+# lessons practiced and learned here:
+-a nested loop 
+
---
+++ main.js
@@ -0,0 +1,46 @@
+const container = document.querySelector(".container");
+const btn = document.querySelector(".btn");
+let size = 16;
+
+const gridCell = (size) => {
+    const wrapper = document.createElement("div");
+    wrapper.classList.add("wrapper");
+
+    for (let i = 0; i < size; i++) {
+        const gridRow = document.createElement("div");
+        gridRow.classList.add("grid-row");
+
+        for (let j = 0; j < size; j++) {
+            const wdAndht = 660 / size;
+            const gridBox = document.createElement('div');
+            gridBox.classList.add('grid-box');
+            gridBox.style.width = `${wdAndht}px`;
+            gridBox.style.height = `${wdAndht}px`;
+            gridBox.addEventListener("mouseover", () => {
+                // gridBox.style.backgroundColor = 'black';
+                gridBox.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`
+            });
+            gridRow.appendChild(gridBox);
+        }
+        wrapper.appendChild(gridRow);
+    }
+    container.appendChild(wrapper);
+};
+
+btn.addEventListener("click", () => {
+    let userSize = Number(prompt("Enter any number between 1-100"));
+
+    while (userSize > 100 || userSize < 1 || isNaN(userSize)) {
+        userSize = Number(prompt("Enter a number between 1 and 100"));
+    }
+
+    const wrapper = document.querySelector(".wrapper");
+    if (wrapper) {
+        wrapper.remove();
+    }
+    
+    gridCell(userSize);
+});
+
+// Initial grid creation
+gridCell(size);
